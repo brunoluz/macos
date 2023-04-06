3ddebfd9a8390f5ba8ef7bb7595150a94bc1fd9a
brunoluz <bruno.al.luz@gmail.com>
2023-04-06 17:32:21 -0300

    Update 01_new_environment.sh

--- 01_new_environment.sh
+++ 01_new_environment.sh
@@ -1,3 +1,3 @@
 # software for managing windows via shortcut
 brew cask install rectangle
-PS1=\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[0;32m\]$(parse_git_branch)\[\033[00m\]$
+PS1='%F{green}%n@%m%f:%F{cyan}%/%f %F{white}%(!.#.$)%f'
